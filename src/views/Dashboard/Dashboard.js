d566a6296ba425f78e998d2074d17967c7c4cbb3
prajjwaljogdand <77375933+prajjwaljogdand@users.noreply.github.com>
2022-03-11 17:28:19 +0530

    Fixed bug for metamask wallet connection

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -3,14 +3,22 @@ import Bonds from './components/Bonds';
 import Page from '../../components/Page';
 import { Switch } from 'react-router-dom';
 import BombFinanceSummary from './components/BombFinanceSummary';
-
+import { useWallet } from 'use-wallet'; 
+import UnlockWallet from '../../components/UnlockWallet';
 const Dashboard = () => {
+  const { account } = useWallet();
   return (
     <>
       <Switch>
         <Page>
+        {!!account ? (
+          <>
           <BombFinanceSummary />
           <Bonds />
+          </>
+           ) : (
+        <UnlockWallet />
+      )}
         </Page>
       </Switch>
     </>
